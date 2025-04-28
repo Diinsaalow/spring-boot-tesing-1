5be7e957fd8a990c229d1ee78188c282460d9d33
Ismail <ismail.haabare@gmail.com>
2025-04-28 10:04:36 +0300

    Project Setup

---
+++ src/test/java/com/spring/exercise_1/Exercise1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.exercise_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
